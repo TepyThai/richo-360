b0649cab6a3d7fccf3970f04db3356f0e945235d
tepythai <thaitepy@gmail.com>
2020-07-02 17:15:10 +0900

    add simple phrase

---
+++ pages/release.js
@@ -0,0 +1,10 @@
+import HelloWorld from '../component/hello-world';
+
+export default function Home() {
+  return (
+    <div className="example">
+      <HelloWorld />
+      <div>ssssssd from TEPY</div>
+    </div>
+  );
+}
